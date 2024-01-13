dfb1b7be6841cd08481d02b058fa67452b6e104b
utsav82 <utsa.yadav.94@gmail.com>
2024-01-13 16:24:42 +0530

    worked on groups

---
+++ app/dashboard/[groupId]/layout.jsx
@@ -0,0 +1,114 @@
+import { ResizablePanel, ResizableHandle, ResizablePanelGroup } from '@/components/ui/resizable'
+import { Separator } from '@/components/ui/separator';
+import { formatDistanceToNow } from 'date-fns';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import Link from 'next/link';
+const layout = ({ params, children }) => {
+    const expenses = [
+        {
+            description: "Grocery Shopping",
+            amount: 50.25,
+            category: "Food",
+            date: new Date("2024-01-15"),
+            group_id: "flat-mates",
+            user_id: "USER_ID_1",
+            shares: [
+                { user_id: "USER_ID_1", share_amount: 25.00 },
+                { user_id: "USER_ID_2", share_amount: 25.00 },
+            ]
+        },
+        {
+            description: "Movie Night",
+            amount: 30.00,
+            category: "Entertainment",
+            date: new Date("2024-01-20"),
+            group_id: "flat-mates",
+            user_id: "USER_ID_2",
+            shares: [
+                { user_id: "USER_ID_2", share_amount: 15.00 },
+                { user_id: "USER_ID_3", share_amount: 15.00 },
+            ]
+        },
+        {
+            description: "Utilities",
+            amount: 80.50,
+            category: "Bills",
+            date: new Date("2024-01-25"),
+            group_id: "flat-mates",
+            user_id: "USER_ID_1",
+            shares: [
+                { user_id: "USER_ID_1", share_amount: 40.25 },
+                { user_id: "USER_ID_2", share_amount: 40.25 },
+            ]
+        },
+        {
+            description: "Grocery Shopping",
+            amount: 50.25,
+            category: "Food",
+            date: new Date("2024-01-15"),
+            group_id: "flat-mates",
+            user_id: "USER_ID_1",
+            shares: [
+                { user_id: "USER_ID_1", share_amount: 25.00 },
+                { user_id: "USER_ID_2", share_amount: 25.00 },
+            ]
+        },
+
+
+    ];
+    return (
+        <ResizablePanelGroup
+            direction="horizontal"
+            className="h-screen container mt-10 items-stretch"
+        >
+            <ResizablePanel defaultSize={50} minSize={30}>
+                <div className="flex items-center px-4 py-2">
+                    <Link
+                        href={`/dashboard/${params.groupId}`} className="text-xl font-bold">{params.groupId}</Link>
+                </div>
+                <Separator />
+                <div className="bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+                </div>
+                <div className="m-0">
+                    <ScrollArea className="h-screen">
+                        <div className="flex flex-col gap-2 p-4 pt-0">
+                            {expenses.map((expense) => (
+                                <Link href={`/dashboard/${expense.group_id}/${expense.id}`}
+                                    key={expense.id}
+                                    className=
+                                    "flex flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent"
+                                >
+                                    <div className="flex w-full flex-col gap-1">
+                                        <div className="flex items-center">
+                                            <div className="flex items-center gap-2">
+                                                <div className="font-semibold">{expense.description}</div>
+                                            </div>
+                                            <div
+                                                className=
+                                                "ml-auto text-xs text-foreground"
+                                            >
+                                                {formatDistanceToNow(new Date(expense.date), { addSuffix: true })}
+                                            </div>
+                                        </div>
+                                        <div className="text-xs font-medium">{expense.category}</div>
+                                    </div>
+                                    <div className="line-clamp-2 text-xs text-muted-foreground">
+                                        {expense.amount.toFixed(2)} USD {/* Assuming 'amount' is a numeric field */}
+                                    </div>
+                                    {expense.groupName && <div className="text-xs text-muted-foreground">{expense.groupName}</div>}
+                                </Link>
+                            ))}
+                        </div>
+                    </ScrollArea>
+                </div>
+
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel defaultSize={50}>
+                {children}
+            </ResizablePanel>
+        </ResizablePanelGroup >
+    )
+}
+
+export default layout
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -22,133 +22,38 @@ export default function DashboardPage() {
   return (
     <>
       <div className="flex-col flex">
-        <div className="flex-1 space-y-4 p-8 pt-6">
-          <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+        <div className="flex-1 space-y-4 p-4 sm:p-8 pt-6">
+          <div className="flex flex-col sm:flex-row items-center justify-between space-y-2">
+            <h2 className="text-2xl sm:text-3xl font-bold tracking-tight">
+              Dashboard
+            </h2>
+
             <div className="flex items-center space-x-2">
               <CalendarDateRangePicker />
-              <Button className="hidden sm:block">Download</Button>
             </div>
           </div>
-
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  Total Expenses
-                </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                </svg>
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">$45,231.89</div>
-                <p className="text-xs text-muted-foreground">
-                  +20.1% from last month
+          <div className="bg-green-200 rounded-lg p-4 sm:p-8 mb-4">
+            <div className="flex items-center justify-between relative">
+              <div>
+                <p className="text-sm text-green-800 font-semibold text-wrap max-w-sm">
+                  Welcome back!
                 </p>
-              </CardContent>
-            </Card>
-
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                 Money owed
-                </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                  <circle cx="9" cy="7" r="4" />
-                  <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                </svg>
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">+2350</div>
-                <p className="text-xs text-muted-foreground">
-                  +180.1% from last month
+                <p className="text-sm text-green-800 font-semibold text-wrap max-w-sm z-1">
+                  Track expenses, manage groups, and stay on top of your
+                  finances effortlessly.
                 </p>
-              </CardContent>
-            </Card>
-
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  Shared Expenses
-                </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <rect width="20" height="14" x="2" y="5" rx="2" />
-                  <path d="M2 10h20" />
-                </svg>
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">+12,234</div>
-                <p className="text-xs text-muted-foreground">
-                  +19% from last month
-                </p>
-              </CardContent>
-            </Card>
-
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  Pending Payments
-                </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                </svg>
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">+573</div>
-                <p className="text-xs text-muted-foreground">
-                  +201 since last hour
-                </p>
-              </CardContent>
-            </Card>
+              </div>
+              <Image
+                src="/dash.png"
+                width={100}
+                height={100}
+                unoptimized={true}
+                className="top--20 right-10 hidden w-32 sm:w-48 sm:block absolute rounded-full z--1"
+              />
+            </div>
           </div>
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-            <Card className="col-span-4">
-              <CardHeader>
-                <CardTitle>Overview</CardTitle>
-              </CardHeader>
-              <CardContent className="pl-2">
-                <Overview />
-              </CardContent>
-            </Card>
-            <Card className="col-span-3">
+          <div className="grid gap-4 sm:grid-cols-3">
+            <Card className="sm:col-span-2">
               <CardHeader>
                 <CardTitle>Recent Expenses</CardTitle>
                 <CardDescription>
@@ -159,6 +64,88 @@ export default function DashboardPage() {
                 <RecentSales />
               </CardContent>
             </Card>
+            <div className="flex flex-col gap-4">
+              <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">
+                    Total Expenses
+                  </CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="h-4 w-4 text-muted-foreground"
+                  >
+                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">$45,231.89</div>
+                  <p className="text-xs text-muted-foreground">
+                    +20.1% from last month
+                  </p>
+                </CardContent>
+              </Card>
+
+              <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">
+                    Money owed
+                  </CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="h-4 w-4 text-muted-foreground"
+                  >
+                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                    <circle cx="9" cy="7" r="4" />
+                    <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">$2350</div>
+                  <p className="text-xs text-muted-foreground">
+                    +180.1% from last month
+                  </p>
+                </CardContent>
+              </Card>
+
+              <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">
+                    Shared Expenses
+                  </CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="h-4 w-4 text-muted-foreground"
+                  >
+                    <rect width="20" height="14" x="2" y="5" rx="2" />
+                    <path d="M2 10h20" />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">$12,234</div>
+                  <p className="text-xs text-muted-foreground">
+                    +19% from last month
+                  </p>
+                </CardContent>
+              </Card>
+            </div>
           </div>
         </div>
       </div>
