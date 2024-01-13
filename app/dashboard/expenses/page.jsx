94427fe56748a6dd5f410d9ed3c624ca31b7de0f
Rishabhluv <rishabhsrivastavaluv@gmail.com>
2024-01-13 16:27:48 +0530

    expenses

--- app/dashboard/expenses/page.jsx
+++ app/dashboard/expenses/page.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
 
+import React from "react";
+import { columns } from "@/components/columns";
+import { DataTable } from "@/components/ui/tabledata";
+
+const data = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  }
+];
 const page = () => {
   return (
-    <div>
-      Expenses page
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
---
+++ components/columns.jsx
@@ -0,0 +1,14 @@
+export const Columns = [
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "amount",
+    header: "Amount",
+  }
+];
