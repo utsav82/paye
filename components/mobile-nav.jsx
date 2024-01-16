631e3c755fd962f4322a39b61fac8e9b97b3f3c0
utsav82 <utsa.yadav.94@gmail.com>
2024-01-16 13:37:15 +0530

    updated side nav

--- components/mobile-nav.jsx
+++ components/mobile-nav.jsx
@@ -1,6 +1,13 @@
 "use client"
 import React, { useState } from 'react';
-import SideNav from './side-nav';
+import { Separator } from "@/components/ui/separator";
+import { Bike, File, Inbox, LayoutDashboard, Home, Users2 } from "lucide-react";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Nav } from "@/components/main-nav";
+import { UserNav } from "@/components/user-nav";
+import CreateGroup from "./create-group-button";
+
+
 const MobileSideNav = () => {
     const [isNavOpen, setNavOpen] = useState(false);
 
@@ -9,8 +16,8 @@ const MobileSideNav = () => {
     };
 
     return (
-        <div className="md:hidden  bg-background z-10 fixed">
-            <button onClick={toggleNav} className="p-3 focus:outline-none">
+        <div className="md:hidden bg-background z-10 fixed">
+            {!isNavOpen && <button onClick={toggleNav} className="p-3 focus:outline-none">
                 <svg
                     className="h-6 w-6 text-gray-600"
                     fill="none"
@@ -22,9 +29,60 @@ const MobileSideNav = () => {
                 >
                     <path d="M4 6h16M4 12h16m-7 6h7"></path>
                 </svg>
-                {/* Side Navigation */}
-                {isNavOpen && <SideNav className={"h-screen ease-in"} />}
-            </button>
+            </button>}
+            {/* Side Navigation */}
+            {isNavOpen && <div className="p-5 h-screen ease-in-out">
+                <div onClick={toggleNav} >
+                    <div className="flex items-center gap-2 mt-5 mb-2">
+                        <UserNav />
+                        <ModeToggle />
+                    </div>
+                    <Nav
+                        links={[
+                            {
+                                title: "Dashboard",
+                                icon: LayoutDashboard,
+                                href: "/dashboard",
+                                variant: "ghost",
+                            },
+                            {
+                                title: "My Expenses",
+                                icon: File,
+                                href: "/dashboard/expenses",
+                                variant: "ghost",
+                            },
+                            {
+                                title: "Expense shares",
+                                icon: Inbox,
+                                href: "/dashboard/expense_share",
+                                variant: "ghost",
+                            },
+                        ]}
+                    />
+                    <Separator />
+                    <div className="group flex gap-4 pl-2 pt-2 m-2 font-semibold items-center">
+                        <Users2 className="h-4 w-4" /> <div> Your Groups</div>
+                    </div>
+                    <Nav
+                        links={[
+                            {
+                                title: "Flat Mates",
+                                icon: Home,
+                                href: "/dashboard/flat-mates",
+                                variant: "ghost",
+                            },
+                            {
+                                title: "Goa Trip",
+                                icon: Bike,
+                                href: "/dashboard/goa-trip",
+                                variant: "ghost",
+                            },
+                        ]}
+                    />
+                </div>
+                <CreateGroup></CreateGroup>
+                <Separator className="hidden sm:block" />
+            </div>}
         </div>
     );
 };
